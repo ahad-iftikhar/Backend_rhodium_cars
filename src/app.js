f0260f118d99d9e8aa543bf130d335092333b9a1
Ahad Iftikhar <ahadiftikhar77@gmail.com>
2025-01-11 08:00:34 +0500

    created user model

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 export { app };
